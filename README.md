d89accbab9d4c0a03f25b9b9821e5adb7fe3b1f8
YendisFish <YendisFish@users.noreply.github.com>
2026-07-05 11:14:02 -0600

    added readme and further improved build script

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sirius
+Sirius is an open source layer over the Linux kernel
+which runs containerized applicaitons and delivers drivers
+to userspace. Using modern languages such as Go, Flux, and
+Rust, Sirius abandons old standards of C and aims for a 
+bright future!
+
+# How to Build
+The dependencies you will need are:
+
+- Go
+- Docker
+- Git
+- Linux/MacOS
+
+If you have all of these dependencies you can run
+the ``dockerbuild.sh`` file and afterwards ``qemu.sh``
+will run it in a VM.
--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,14 +1,78 @@
 #!/bin/bash
 
+function initcorefs {
+    mkdir ./build/sirius-root/
+
+    ROOT="./build/sirius-root"
+
+    mkdir -p $ROOT/etc/alpha/
+    mkdir -p $ROOT/proc/
+    mkdir -p $ROOT/sys/
+    mkdir -p $ROOT/dev/
+    mkdir -p $ROOT/var/log/alpha/
+
+    mkdir -p $ROOT/apps/
+
+    mknod -m 600 $ROOT/dev/console c 5 1
+    mknod -m 666 $ROOT/dev/null c 1 3
+    mknod -m 666 $ROOT/dev/zero c 1 5
+
+cat <<EOF > $ROOT/etc/alpha/alpha.toml
+[isolated]
+# Rules for containers (namespaces, cgroups, etc)
+
+[host]
+# Rules for core services (compositor, init-bridge)
+ls = {
+    name = "/bin/ls",
+    args = "/"
+}
+EOF
+
+    # Inject packages
+
+    # mkdir -p $ROOT/apps/fun/
+    # mkdir -p $ROOT/apps/fun/data/
+    # GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -C ./fun --ldflags="-s -w" -o ../build/sirius-root/apps/fun/fun .
+    # cp ./fun/package.toml $ROOT/apps/fun/
+}
+
+function buildinit {
+    mkdir ./build/sirius-root/
+
+    echo "Building Sirius..."
+    GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -C ./alpha --ldflags="-s -w" -o ../build/sirius-root/init .
+    chmod +x ./build/sirius-root/init
+
+    initcorefs
+
+    cd ./build/sirius-root/
+    find . -print0 | cpio --null -ov --format=newc > ../initrd.cpio
+    cd ../
+
+    gzip initrd.cpio
+
+    cd $root
+}
+
+function full_reset {
+    rm -rf ./build/
+    mkdir ./build/sirius-root/
+    mkdir ./build/kernelb/
+    mkdir ./build/kernel/boot/
+}
+
+function reset_tools {
+    rm -rf ./build/sirius-root/
+    rm ./build/initrd.cpio.gz
+    mkdir ./build/sirius-root/
+}
+
 root=$(pwd)
 
 if [ ! -z "$1"  ] && [ "$1" == "full" ]; then
-    rm -rf ./build/
-    mkdir ./build/sirius-root/
-    rm -rf ./build/kernelb/
-    rm -rf ./build/kernel/
-    mkdir ./build/kernelb/
-    mkdir ./build/kernel/boot/
+    full_reset
+
     git clone https://github.com/torvalds/linux ./build/kernelb/ --depth 1
     cp kernel.config "./build/kernelb/.config"
     docker build -t sirius .
@@ -17,41 +81,14 @@ if [ ! -z "$1"  ] && [ "$1" == "full" ]; then
     mkdir ./build/kernel/boot/
     cp ./build/kernelb/arch/arm64/boot/** ./build/kernel/boot/
 
-    echo "Building Sirius..."
-    GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -C ./alpha --ldflags="-s -w" -o ../build/sirius-root/init .
-    chmod +x ./build/sirius-root/init
-
-    ./initcorefs.sh
-
-    cd ./build/sirius-root/
-    find . -print0 | cpio --null -ov --format=newc > ../initrd.cpio
-    cd ../
-
-    gzip initrd.cpio
-
-    cd $root
+    buildinit
     echo "Done!"
 fi
 
 
 if [ ! -z "$1"  ] && [ "$1" == "tools" ]; then
-    rm -rf ./build/sirius-root/
-    rm ./build/initrd.cpio.gz
+    reset_tools
+    buildinit
 
-    mkdir ./build/sirius-root/
-
-    echo "Building Sirius..."
-    GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -C ./alpha --ldflags="-s -w" -o ../build/sirius-root/init .
-    chmod +x ./build/sirius-root/init
-
-    ./initcorefs.sh
-
-    cd ./build/sirius-root/
-    find . -print0 | cpio --null -ov --format=newc > ../initrd.cpio
-    cd ../
-
-    gzip initrd.cpio
-
-    cd $root
     echo "Done!"
 fi
